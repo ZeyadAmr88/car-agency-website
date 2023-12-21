4a1aa71178628a4fb7c19e8404ba8e38f41f8fc9
zeyadamr8 <96020833+ZeyadAmr88@users.noreply.github.com>
2023-12-22 01:51:10 +0200

    setup node, epress and db

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("upload"));
+const cors = require("cors");
+app.use(cors());
+
+const auth = require("./routes/Auth");
+const cars = require("./routes/Cars");
+
+app.listen(4000, "localhost", () => {
+  console.log("server is running");
+});
+
+app.use("/auth", auth);
+app.use("/cars", cars);
---
+++ routes/Cars.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const conn = require("../db/dbConnection");
+
+module.exports = router;
