fd245ab9adc0e33a3e71219e249090a1e347e639
sandynazeh <sandy_166@icloud.com>
2023-12-22 18:31:54 +0200

    added Registration form

--- client/src/Pages/Auth/Login.js
+++ client/src/Pages/Auth/Login.js
@@ -2,20 +2,23 @@
 import react from"react";
 import'../../css/Login.CSS';
 const Login =() =>{
-   return (<div className="login-container">
+   return (
+   <div className="login-container">
     <h1>Login Form</h1>
    
 
     <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+    
+
+      <Form.Group className="mb-1" >
         <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="please enter your email" />
        
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-2s" >
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" placeholder=" please enter your Password" />
       </Form.Group>
       
       <Button variant="primary" type="submit">
