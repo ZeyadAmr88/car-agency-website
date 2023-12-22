7207cf1b642e62b9e7c10ca7a86a8c5a0d563db0
dania-20 <you@example.com>
2023-12-22 19:13:12 +0200

    header& footer😡

--- client/src/shared/Header.js
+++ client/src/shared/Header.js
@@ -1,7 +1,53 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import{Link} from "react-bootstrap-dom";
+import{ removeAuthUser, getAuthUser} from "../helper/storage";
+import{useNavigare} from "react-router-dom"
 
 const Header = () => {
-  return <div>header</div>;
+  const navigate =useNavigate();
+  const auth= getAuthUser();
+  console.log();
+  const Logout= ()=> {
+    removeAuthUser();
+    navigate("/");
+  }
+  return <>
+  <Navbar bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand href="#home">CAR AGENCY</Navbar.Brand>
+      <Nav className="me-auto">
+        <Link className="nav=link" to={'/'}>Home</Link>
+        <Link className="nav=link" to={'/admin'}>Admin</Link>
+
+        {!auth&&(
+          <>
+           <Link className="nav=link"  to={'/login'}>Login</Link>
+           <Link className="nav=link" to={'/register'}>Register</Link>
+           </>
+        )}
+
+
+        {auth&& auth.role ===1 &&(
+          <>
+           <Link className="nav=link"  to={'/manage-cars'}>Manage Cars</Link>
+           </>
+        )}
+
+
+      <Nav className="ms-auto">
+        {auth && <Nav.Link onClick={Logout}>Logout</Nav.Link>}
+
+      </Nav>
+      
+        </Nav>
+    </Container>
+  </Navbar>
+
+    </>;
+
 };
 
 export default Header;
